9e73aa2c4865aede0ec7993cbc95456537fca3ce
Deepak Kumar <d1612k@gmail.com>
2019-08-10 17:08:52 +0530

    added stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,47 @@
 <template>
-  <h1>Stock</h1>
+  <div class="col-sm-6 col-md-4 mt-4 mb-2">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price}})</small>
+        </h3>
+      </div>
+      <div class="card-body">
+        <div class="pull-left">
+          <input type="number" class="form-control" placeholder="quantity" v-model="quantity" />
+        </div>
+        <div class="pull-right mt-3">
+          <button class="btn btn-success" :disabled="quantity <= 0" @click="stockPurchase">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    stockPurchase() {
+      const data = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        stockQuantity: this.quantity
+      };
+      this.quantity = 0;
+    }
+  },
+  props: ["stock"]
+};
+</script>
+
+<style scoped>
+small {
+  font-size: 1.2rem;
+}
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Stocks</h1>
+  <div class="row">
+    <stock v-for="stock in stocks" :key="stock.id" :stock="stock"></stock>
+  </div>
 </template>
+
+<script>
+import Stock from "./Stock.vue";
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: "1", name: "BMW", price: "99" },
+        { id: "2", name: "Coca-cola", price: "45" },
+        { id: "3", name: "Pepsico", price: "85" },
+        { id: "4", name: "Fiat", price: "15" },
+        { id: "5", name: "Google", price: "3000" }
+      ]
+    };
+  },
+  components: {
+    stock: Stock
+  }
+};
+</script>
